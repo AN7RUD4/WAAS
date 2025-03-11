3bed20dd70716344aabeb551b9b1aa398d12ebc4
Anirudh Anilkumar <anirudh.anilkumar@gmail.com>
2025-03-11 20:33:53 +0530

    sorted backend and front end

---
+++ frontend/android/app/src/main/kotlin/com/example/user_samp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.user_samp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ frontend/lib/colors/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = Colors.green;
+Color secondaryColor = Colors.white;
+Color bgColor = Colors.white;
