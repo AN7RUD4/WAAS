3690caa12956365c8bd1ab28b91502f5257ec4b8
Anirudh Anilkumar <anirudh.anilkumar@gmail.com>
2025-04-03 22:51:26 +0530

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -41,6 +41,7 @@ const storage = multer.diskStorage({
 });
 const fileFilter = (req, file, cb) => {
   const allowedTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/bmp'];
+  console.log('File MIME type:', file.mimetype); // Log the MIME type
   if (allowedTypes.includes(file.mimetype)) {
     cb(null, true);
   } else {
